b4b9784a0eb51cd3e37a3a7bea3fd0db3588b2ac
medardoc <m.carpio2@alumnos.santotomas.cl>
2024-09-25 14:02:50 -0300

    En este commit podemos observar toda la integración de nuestro proyecto.

--- app/src/main/java/com/example/aplicacioniheartsmartpro/WelcomeActivity.java
+++ app/src/main/java/com/example/aplicacioniheartsmartpro/WelcomeActivity.java
@@ -4,22 +4,39 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class WelcomeActivity extends AppCompatActivity {
 
+    private Button btnEnterPanel;
+    private ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome); // Asocia con el layout activity_welcome.xml
+        setContentView(R.layout.activity_welcome);
 
-        Button btnEnterPanel = findViewById(R.id.btnEnterPanel);
+        btnEnterPanel = findViewById(R.id.btnEnterPanel);
+        progressBar = findViewById(R.id.progressBar);
+
         btnEnterPanel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Cambiar a MainActivity al hacer clic
-                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
-                startActivity(intent);
+                // Muestra el ProgressBar y oculta el botón
+                btnEnterPanel.setVisibility(View.INVISIBLE);
+                progressBar.setVisibility(View.VISIBLE);
+
+                // Simula una acción (por ejemplo, un retraso de 2 segundos) y luego cambia a MainActivity
+                new android.os.Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish(); // Cierra la actividad actual
+                    }
+                }, 2000); // 2 segundos de retraso
             }
         });
     }
